32660549866befe0b6ed9a6f6b3c80fa8439476d
GoodManWEN <44322449+GoodManWEN@users.noreply.github.com>
2021-11-30 01:41:18 +0000

    Update nlp module & replace frontend vue framework

---
+++ cmt_nlp.py
@@ -0,0 +1,92 @@
+import os
+import datetime
+import jieba
+import pprint
+from collections import defaultdict
+from gensim import corpora
+from gensim import models
+from gensim import similarities
+from pipeit import *
+
+jieba.load_userdict(os.path.abspath('./nlp/jieba_userdict.txt'))
+STOPLIST = set('的,了,个,呢,呀,哈,哈哈,哈哈哈,哈哈哈哈,哈哈哈哈哈,哈哈哈哈哈哈,啦,？,?,！,，,(,),（,）,【,】, ~,",“,”,‘,’,—,-,。,.,啊,阿,吗,喂, '.split(','))
+
+def split_string(string):
+    return [_ for _ in jieba.lcut_for_search(string) if _ not in STOPLIST]
+
+def load_processed_corpus_single_file(file_path, filter=False, debug=False):
+    texts = (
+        Read(file_path).split('\n')[6:] 
+        | Map(lambda x:(split_string(x[22:]), x[22:]))
+        | Filter(lambda x: x[0] != [])
+        | list
+    )
+    if filter:
+        frequency = defaultdict(int)
+        for text, _ in texts:
+            for token in text:
+                frequency[token] += 1
+        processed_corpus = [([token for token in text if frequency[token] > 1], source) for text, source in texts] | Filter(lambda x:len(x[0]) > 1) | list
+        if debug: pprint.pprint(processed_corpus)
+        return processed_corpus
+    else:
+        return texts
+
+def flt_uncommon(processed_corpus):
+    frequency = defaultdict(int)
+    for text, _ in processed_corpus:
+        for token in text:
+            frequency[token] += 1
+    processed_corpus = [([token for token in text if frequency[token] > 1], source) for text, source in processed_corpus] | Filter(lambda x:len(x[0]) > 1) | list
+    return processed_corpus
+
+def train_nlp_model(src_dir, file_name, passes = 5, file_count = 3):
+    src_files = []
+    for src_files in os.walk(src_dir):
+        src_files = (
+            src_files[2] 
+            | Filter(lambda x:x[:5] == 'danmu' and os.path.splitext(x)[1] == '.txt') 
+            | Map(lambda x: os.path.join(src_dir, x))
+            | Filter(lambda x: os.stat(x).st_size >= 1024) 
+            | list
+        ); break
+    src_files.sort(key = lambda x:datetime.datetime.strptime(os.path.split(x)[1][6:6+19], '%Y-%m-%d-%H-%M-%S'), reverse = True)
+    
+    file_path = os.path.join(src_dir, file_name)
+
+    processed_corpus = []
+    snake_idx, count, positive = 0, 0, -1
+    if file_path in src_files:
+        processed_corpus.extend(load_processed_corpus_single_file(file_path))
+        snake_idx = src_files.index(file_path)
+        src_files.remove(file_path)
+        count += 1
+    
+    while count < file_count:
+        snake_idx= snake_idx - 1 if positive % 3==1 else -snake_idx
+        positive += 1
+        count += 1
+        try:
+            processed_corpus.extend(load_processed_corpus_single_file(src_files))
+            count += 1
+        except:
+            ...
+    processed_corpus = flt_uncommon(processed_corpus)
+
+    dictionary = corpora.Dictionary(processed_corpus | Map(lambda x:x[0]))
+    bow_corpus = [dictionary.doc2bow(text) for text, _ in processed_corpus]
+    lda = models.LdaModel(bow_corpus, id2word=dictionary, num_topics=100, passes = passes)
+    index = similarities.SparseMatrixSimilarity(lda[bow_corpus], num_features=120)
+    return processed_corpus, lda, index, dictionary
+
+def string_query(string, processed_corpus, model, index, dictionary):
+    query_bow = dictionary.doc2bow(split_string(string))
+    sims = index[model[query_bow]]
+    sims_top = sorted(enumerate(sims), key=lambda x: x[1], reverse=True)[:10]
+    if len(sims_top) < 10:
+        raise 
+    documents_top = [processed_corpus[document_number][1] for document_number, score in sims_top]
+    res = list(set(documents_top))
+    res.sort(key = documents_top.index)
+    res = ' '.join(res[:6] | Map(lambda x:f"#{x}#"))
+    return res
--- render.py
+++ render.py
@@ -4,7 +4,9 @@ from pyecharts.globals import ThemeType
 import datetime
 from typing import List, Optional
 from bs4 import BeautifulSoup
+from cmt_nlp import train_nlp_model, string_query
 from pipeit import *
+import json
 import os
 
 def render(x_axis:List[str], yll:List[Optional[float]], ylh:List[Optional[float]], avg: float, cmt_length, time_length, avg_cmt, file_name):
@@ -51,7 +53,7 @@ def render(x_axis:List[str], yll:List[Optional[float]], ylh:List[Optional[float]
     grid.add(line, grid_opts = opts.GridOpts(pos_left='4%', pos_right='4%'))
     return grid
 
-def jinja222(extra_head, main_chart_div, ptao, extra_scripts):
+def jinja222(extra_head, main_chart_div, ptao, extra_scripts, tag_res):
     template = Read(os.path.abspath('./templates/html22-2.html'))
     tr_tmplate ='''
                     <tr>
@@ -80,6 +82,7 @@ def jinja222(extra_head, main_chart_div, ptao, extra_scripts):
         .replace('{{main_chart_div}}', str(main_chart_div))
         .replace('{{trs}}', str(trs))
         .replace('{{extra_scripts}}', str(extra_scripts))
+        .replace('{{tag_res_data}}', f"let jsonStr={repr(json.dumps(tag_res))}")
     )
     return template
 
@@ -97,6 +100,7 @@ def jinja22(fl):
     template = template.replace('{{filelist_tr}}', filelist_tr)
     return template
 
+
 def main():
     src_dir = os.path.abspath('./data')
     dst_dir = os.path.abspath('./docs')
@@ -113,12 +117,19 @@ def main():
     diff_files = sorted(src_files.difference(dst_files) | Map(lambda x:x+'.txt') | list)
     drop_list = []
     for file_name in diff_files:
+
         file_path = os.path.join(src_dir, file_name)
         text = Read(file_path).split('\n') | Filter(lambda x:x.strip() != '') | list
         st_time = datetime.datetime.strptime(text[3], '%Y-%m-%d %H:%M:%S')
-        if len(text) <= 10:
+        if len(text) <= 50:
             drop_list.append(file_path)
             continue
+        '''
+        理论上应该是公用同一字典即可学习，但是测试了一下发现效果居然受语料库输入顺序影响，也是搞不太清楚为什么
+        '''
+        processed_corpus, lda, index, dictionary = train_nlp_model(src_dir, file_name, passes = 1, file_count = 1)
+
+
         text_backup = text[min(len(text) - 1, 5):] | Map(lambda x: (datetime.datetime.strptime(x[:19], '%Y-%m-%d %H:%M:%S'), x[22:])) | list 
         text = text[min(len(text) - 1, 5):] | Map(lambda x: (datetime.datetime.strptime(x[:19], '%Y-%m-%d %H:%M:%S') - st_time).total_seconds()) | list
         cmt_length = len(text)
@@ -203,6 +214,42 @@ def main():
                     ptao.append((f"{str(int(idx*5//3600)).zfill(2)}:{str(int((((idx*5)%3600)//60))).zfill(2)}:{str(int(((idx*5)%60))).zfill(2)}", str(st_time + datetime.timedelta(seconds = idx*5)), cmt_max, _buffer))
                 last_status3 = tab[idx] >= avg3
 
+        # nlp topic part
+        if len(ptao) > 1:
+            last_he_time = datetime.datetime.strptime(ptao[0][0],'%H:%M:%S')
+            show_text_list = [(last_he_time, ptao[0][2]),]
+            anchor_time = datetime.datetime.strptime('0:00:00','%H:%M:%S')
+            for higherg in ptao[1:]:
+                curt_he_time = datetime.datetime.strptime(higherg[0],'%H:%M:%S')
+                t_diff = (curt_he_time - last_he_time).total_seconds()
+                if t_diff >= 60 * 6.5:
+                    # 与上次高能间隔六分钟以上
+                    show_text_list.append((curt_he_time, higherg[2]))
+                last_he_time = curt_he_time
+            show_text_list = show_text_list | Map(lambda x: (int((x[0] - anchor_time).total_seconds() // 5), x[1])) | list 
+            if show_text_list:
+                ctime_range_list = []
+                for sidx, max_cmt in show_text_list:
+                    for cidx in range(sidx, min(sidx + 10, len(tab)), 1):
+                        if tab[cidx] == max_cmt:
+                            break
+                    else:
+                        # 理论上这种情况不应该发生
+                        raise 
+                    converted_time_range = (st_time + datetime.timedelta(seconds = (cidx-3) * 5), st_time + datetime.timedelta(seconds = (cidx+8) * 5), cidx)
+                    ctime_range_list.append(converted_time_range)
+                cmt_cur = 0
+                tag_res = []
+                for converted_time_st, converted_time_ed, cidx in ctime_range_list:
+                    content_lst = []
+                    while cmt_cur < len(text_backup) and text_backup[cmt_cur][0] <= converted_time_ed:
+                        if text_backup[cmt_cur][0] >= converted_time_st:
+                            content_lst.append(text_backup[cmt_cur][1])
+                        cmt_cur += 1
+                    content = '，'.join(content_lst)
+                    tags = string_query(content, processed_corpus, lda, index, dictionary)
+                    seconds = cidx * 5
+                    tag_res.append((tags,(int(seconds // 3600), int((seconds%3600) // 60), int(seconds%60))))
 
         chart_html = render(x_axis,  yll,  ylh,  avg, cmt_length, time_length, avg_cmt, file_name).render_embed()
         chart_soup = BeautifulSoup(chart_html, 'lxml')
@@ -211,7 +258,7 @@ def main():
         chart_script = chart_soup.find_all('script')[-1]
         
 
-        rendered_html = jinja222(chart_head, chart_div, ptao, chart_script)
+        rendered_html = jinja222(chart_head, chart_div, ptao, chart_script, tag_res)
         Write(os.path.join(dst_dir, os.path.splitext(file_name)[0] + '.html'), rendered_html)
 
     for file_name in drop_list:
